dee3db2303c697a714a9f57a8db17cb3da0cfa7e
Igor de Almeida <cmte.igor.almeida@gmail.com>
2015-11-29 01:10:11 -0300

    *

--- src/main/java/com/jid/daos/LojaRepository.java
+++ src/main/java/com/jid/daos/LojaRepository.java
@@ -3,10 +3,12 @@ package com.jid.daos;
 import com.jid.models.Loja;
 import com.jid.models.Usuario;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by igor on 28/11/15.
  */
-public interface LojaRepository extends CrudRepository<LojaRepository, Long> {
+@Transactional
+public interface LojaRepository extends CrudRepository<Loja, Long> {
     Loja findByUsuario(Usuario usuario);
 }
