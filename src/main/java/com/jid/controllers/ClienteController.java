81d08edec628cf3b9f8d833521458e5ba91c2823
John Gomes <silva.johngomes@gmail.com>
2022-09-04 01:18:46 -0300

    docker componse

--- src/main/java/com/jid/controllers/ClienteController.java
+++ src/main/java/com/jid/controllers/ClienteController.java
@@ -8,7 +8,6 @@ import com.jid.service.ClienteService;
 import com.jid.service.SessionService;
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
